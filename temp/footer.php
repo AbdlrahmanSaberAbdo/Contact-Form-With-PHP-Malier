1bfa31c3ea16de05144fe1650bfbb3c3cfec3352
abdlrahman <abdlrahmansaber2@gmail.com>
2017-07-22 20:19:09 -0700

    Contact Form with PHP Malier

---
+++ temp/footer.php
@@ -0,0 +1,59 @@
+		<!--==================footer=================-->
+		<section class="footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-sm-12 fir">
+						<h3 class="map">
+							<i class="fa fa-map-o fa-lg"></i>
+							Visit <span>Us</span>
+						</h3>
+						<iframe class="map1" src="https://www.google.com.eg/maps/d/embed?mid=1q8Hc114BR7sRIBA3cs7Q-dgiuMI" width="200" height="250"></iframe>
+					</div>	
+
+					<div class="col-md-6 col-sm-12 sec">
+						<h3>
+							<i class="fa fa-rss fa-lg"></i>
+                            Contact <span>Us</span>	
+						</h3>
+						<ul class="list-unstyled fontt">
+							<li class="f"><i class="fa fa-map-marker "></i>  : 29 a st Elachid-El-maniel</li>
+							<li class="f"><i class="fa fa-phone">		</i> : +201202262457 / 01146302286</li>
+							<li class="f"><i class="fa fa-whatsapp">	</i> : +201202262457 / 01146302286</li>
+							<li class="f"><i class="fa fa-envelope r"></i>   : abdlrahmansaber2@gmail.com </li>
+							<li>
+								<ul class="list-unstyled ff">
+									<li><a href="https://www.facebook.com/mahmood.puomy"><i class="fa fa-facebook fa-lg">   </i></a></li>
+									<li><a href="#"><i class="fa fa-twitter fa-lg">    </i></a></li>
+									<li><a href="#"><i class="fa fa-google-plus fa-lg"></i></a></li>
+									<li><a href="#"><i class="fa fa-whatsapp fa-lg">   </i></a></li>
+									<li><a href="#"><i class="fa fa-behance fa-lg">    </i></a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div>
+				<hr>
+			</div>
+				<div class="copy-right text-center">
+					<h3>All Copyrights Reserved By <span>SpeedySofware INC.</span> 2015-2016. Developed By <span>SpeedySofware</span></h3>
+				</div>
+		</section>
+		<!--==================End footer=============-->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    
+    <!-- STick Plugin -->
+    <script src = "lib/stickyjs/sticky.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
+    <!-- nicescroll Plugin -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.4/jquery.nicescroll.min.js"></script>
+    <!-- custom js -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
---
+++ temp/navigate.php
@@ -0,0 +1,19 @@
+  <header id="header" class="header">
+    <div class="container">
+      <div id="logo" class="pull-left">
+        <a class = "navbar-brand" href="index.php">Spee<span>dy </span>So<span>ft</span>Ware</a>
+      </div>
+        
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+            <li><a href="index.php">Home</a></li>
+          <li><a href="about.php">About Us</a></li>
+          <li><a href="portfolio.php">Portfolio</a></li>
+          <li><a href="contactUs.php">Contact Us</a></li>
+      </nav><!-- #nav-menu-container -->
+      
+      <nav class="nav social-nav pull-right hidden-sm-down social">
+        <a href="#"><i class="fa fa-twitter"></i></a> <a href="#"><i class="fa fa-facebook"></i></a> <a href="#"><i class="fa fa-linkedin"></i></a> <a href="#"><i class="fa fa-envelope"></i></a>
+      </nav>
+    </div>
+  </header><!-- #header -->
